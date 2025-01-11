f4483aee473b0c7d8e39abdb5fa94745b1f8cec5
Danny Festor <danny.festor@gmail.com>
2025-01-11 16:03:48 +0900

    WIP

--- src/scripts/darkmode.ts
+++ src/scripts/darkmode.ts
@@ -1,34 +1,34 @@
 const html = document.documentElement;
 
 document.addEventListener("DOMContentLoaded", () => {
-	const darkmode = localStorage.getItem("darkmode");
+  const darkmode = localStorage.getItem("darkmode");
 
-	if (
-		darkmode === "true" ||
-		(darkmode === null &&
-			window.matchMedia &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches)
-	) {
-		html.classList.add("dark");
-		const darkmodeToggle: HTMLInputElement | null = document.querySelector(
-			"#darkmode-toggle input",
-		);
-		if (darkmodeToggle) {
-			darkmodeToggle.checked = true;
-		}
-	}
+  if (
+    darkmode === "true" ||
+    (darkmode === null &&
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    html.classList.add("dark");
+    const darkmodeToggle: HTMLInputElement | null = document.querySelector(
+      "#darkmode-toggle input",
+    );
+    if (darkmodeToggle) {
+      darkmodeToggle.checked = true;
+    }
+  }
 });
 
 const darkmodeToggle = document.querySelector("#darkmode-toggle input");
 
 if (darkmodeToggle) {
-	darkmodeToggle.addEventListener("change", (event) => {
-		if ((event.target as HTMLInputElement).checked) {
-			html.classList.add("dark");
-			localStorage.setItem("darkmode", "true");
-		} else {
-			html.classList.remove("dark");
-			localStorage.setItem("darkmode", "false");
-		}
-	});
+  darkmodeToggle.addEventListener("change", (event) => {
+    if ((event.target as HTMLInputElement).checked) {
+      html.classList.add("dark");
+      localStorage.setItem("darkmode", "true");
+    } else {
+      html.classList.remove("dark");
+      localStorage.setItem("darkmode", "false");
+    }
+  });
 }
--- src/scripts/getTags.ts
+++ src/scripts/getTags.ts
@@ -1,17 +1,17 @@
 import type { AnyEntryMap, CollectionEntry } from "astro:content";
 
 export const getTags = <T extends CollectionEntry<keyof AnyEntryMap>>(
-	_data: T[],
+  _data: T[],
 ): string[] => {
-	const tags = new Set<string>();
+  const tags = new Set<string>();
 
-	const data = _data.filter((item) => item.data?.tags);
+  const data = _data.filter((item) => item.data?.tags);
 
-	for (const item of data) {
-		for (const tag of item.data.tags as string[]) {
-			tags.add(tag);
-		}
-	}
+  for (const item of data) {
+    for (const tag of item.data.tags as string[]) {
+      tags.add(tag);
+    }
+  }
 
-	return Array.from(tags);
+  return Array.from(tags);
 };
