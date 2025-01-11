5b4bfca0f9e27721d19ff2a07b4c27eff5b5e29b
Danny Festor <danny.festor@gmail.com>
2025-01-11 16:24:54 +0900

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dannyfestor.github.io",
+    site: "https://DannyFestor.github.io",
 	integrations: [mdx(), sitemap()],
 
 	markdown: {
